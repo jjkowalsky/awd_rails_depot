f5c93f378ec867d066310dc09b40c26e201010cb
Joe Kowalsky <jjkowalsky@gmail.com>
2013-06-24 01:33:52 -0700

    Catalog playtime

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Depot::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.i18n.available_locales = [:en, :fr]
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,7 @@ Depot::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.i18n.available_locales = [:en, :fr]
+
 end
